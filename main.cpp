e57cdb00f3f3498fe5dccf1804f2e76ca25aa21c
Dominik Utter <Dominik.Utter@bmw.de>
2024-11-21 12:40:12 +0100

    add ignore File and main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int maxDurations = 10;
+
+    for (int i = 0; i < maxDurations; i++)
+    {
+        cout << "Hello " << i << endl;
+    }
+}
